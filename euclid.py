207c616362001382759c00a4c338971229b40462
shiho629 <s.wakuda.24.10.1588@4nonome.com>
2025-03-06 15:51:00 +0900

    関数の課題

--- euclid.py
+++ euclid.py
@@ -2,6 +2,19 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
-while b != 0:
-        a,b = b, int(a) % int(b)
-print('最大公約数：{}'.format(a)) 
+def gcd(a,b):
+        while b != 0:
+                a,b = b, int(a) % int(b)
+        return a
+result3 = gcd(a,b)
+print('問3 最大公約数：{}'.format(result3))
+
+def coprime(a,b):
+        while b !=0:
+                a,b = b, int(a) % int(b)
+        return a == 1
+result4 = coprime(a,b)
+if result4:
+        print(f'問4 {a}と{b}は互いに素')
+else:
+        print(f'問4 {a}と{b}は互いに素ではない')
--- prime_number.py
+++ prime_number.py
@@ -2,18 +2,22 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
-for x in [a,b]:
-    if int(x) < 2:
-        print('{}は素数ではない'.format(x))
-        continue
+def check_natural(n):
+    if not n.isdigit() or int(n)<0:
+        raise ValueError('{}は自然数ではありません'.format(n))
+check_natural(a)
+check_natural(b)
 
-    is_prime = True
-    for i in range(2,int(x)):
-        if int(x) % i ==0:
-            is_prime = False
-            break
-    
-    if is_prime:
+def is_prime(n):
+    if int(n) < 2:
+        return False
+    for i in range(2,int(n)):
+        if int(n) % i == 0:
+            return False
+    return True
+
+for x in [a,b]:
+    if is_prime(x):
         print('{}は素数'.format(x))
     else:
         print('{}は素数ではない'.format(x))
